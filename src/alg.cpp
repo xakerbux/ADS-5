d704907543df28991071e5032dc45ca324f7ea83
xakerbux <156354805+xakerbux@users.noreply.github.com>
2026-06-06 22:51:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,97 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int getPriority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(const std::string& inf) {
+    TStack<char, 100> stack;
+    std::string result;
+    
+    for (size_t i = 0; i < inf.length(); ++i) {
+        char c = inf[i];
+        
+        if (c == ' ') {
+            continue;
+        }
+        
+        if (isdigit(c)) {
+            while (i < inf.length() && isdigit(inf[i])) {
+                result += inf[i];
+                ++i;
+            }
+            result += ' ';
+            --i;
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (c == ')') {
+            while (!stack.isEmpty() && stack.get() != '(') {
+                result += stack.pop();
+                result += ' ';
+            }
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!stack.isEmpty() && getPriority(stack.get()) >= getPriority(c)) {
+                result += stack.pop();
+                result += ' ';
+            }
+            stack.push(c);
+        }
+    }
+    
+    while (!stack.isEmpty()) {
+        result += stack.pop();
+        result += ' ';
+    }
+    
+    if (!result.empty() && result.back() == ' ') {
+        result.pop_back();
+    }
+    
+    return result;
+}
+
+int eval(const std::string& post) {
+    TStack<int, 100> stack;
+    
+    for (size_t i = 0; i < post.length(); ++i) {
+        char c = post[i];
+        
+        if (c == ' ') {
+            continue;
+        }
+        
+        if (isdigit(c)) {
+            int num = 0;
+            while (i < post.length() && isdigit(post[i])) {
+                num = num * 10 + (post[i] - '0');
+                ++i;
+            }
+            stack.push(num);
+            --i;
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            int b = stack.pop();
+            int a = stack.pop();
+            int res = 0;
+            
+            if (c == '+') {
+                res = a + b;
+            } else if (c == '-') {
+                res = a - b;
+            } else if (c == '*') {
+                res = a * b;
+            } else if (c == '/') {
+                res = a / b;
+            }
+            
+            stack.push(res);
+        }
+    }
+    
+    return stack.pop();
 }

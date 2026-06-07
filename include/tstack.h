c634df6ca3607c297201e5fb2850992b7be3ad6e
xakerbux <156354805+xakerbux@users.noreply.github.com>
2026-06-07 23:55:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,39 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int N>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[N];
+    int top;
+
+ public:
+    TStack() : top(-1) {}
+
+    void push(const T& item) {
+        if (top < N - 1) {
+            data[++top] = item;
+        }
+    }
+
+    T pop() {
+        if (top >= 0) {
+            return data[top--];
+        }
+        return T();
+    }
+
+    T get() const {
+        if (top >= 0) {
+            return data[top];
+        }
+        return T();
+    }
+
+    bool isEmpty() const {
+        return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
